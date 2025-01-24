890505ae87cad690d9ed72c95023b204ecb6bf43
rajanikumari180681 <rajanikumari180681@gmail.com>
2025-01-24 14:41:59 +0530

    Add files via upload
    Commiting First

---
+++ src/test/java/demo/Class1.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi i am Rajini");
+		System.out.println("I am a software tester");
+
+	}
+
+}
